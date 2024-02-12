7afba97922871830a32588ef2244b97e93144512
fabiano.capobianchi <fabiano.capobianchi@agid.gov.it>
2024-02-12 10:15:16 +0100

    Pulizia progetto

--- deployAssets.js
+++
@@ -1,7 +0,0 @@
-var cpr = require('cpr');
-
-cpr('node_modules/bootstrap-italia/dist/', 'www/assets/bootstrap-italia/', {
-    deleteFirst: true,
-    overwrite: true,
-    confirm: true
-}, function(err, files) {});
